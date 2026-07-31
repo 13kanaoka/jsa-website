4c0794907aec012df73c2bce5c38ea9996269c36
Matthew Rohde <mrohde213@gmail.com>
2026-07-31 15:51:47 +0900

    feat(header): add header component template with jsa logo

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import jsaLogo from '../assets/jsa-logo.jpg'
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="site_header">
+
+
+        </header>
+    )
+}
+
+export default Header
