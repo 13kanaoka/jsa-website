4b33a5bff1c0c1b167eae25f275db27cb93b2efa
Matthew Rohde <mrohde213@gmail.com>
2026-08-01 13:54:59 +0900

    feat(about): implement about page with slogan, history, and club events

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return <>
     <Header />
     <Home />
+    <About />
   </>
 }
 
