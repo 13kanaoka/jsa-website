d9b0c35589932755f6411b3b4d49427f9163fde8
Matthew Rohde <mrohde213@gmail.com>
2026-08-03 18:04:09 +0900

    chore(vercel): implement vercel analytics tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 import Header from './components/Header'
 import Home from './pages/home-page/Home'
 import About from './pages/about-page/About'
